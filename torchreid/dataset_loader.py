aab437220cfd84a34326cee5d37466e9ee856376
aytac <aytac.kanaci@gmail.com>
2019-01-31 22:26:44 +0000

    Base Classification Dataset

--- torchreid/dataset_loader.py
+++ torchreid/dataset_loader.py
@@ -47,6 +47,24 @@ class MultiScaleImageDataset(Dataset):
         return imgs, pid, camid, img_path
 
 
+class ClassficationDataset(Dataset):
+    """Image Person ReID Dataset"""
+    def __init__(self, dataset, transform=None):
+        self.dataset = dataset
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        img_path, pid, camid = self.dataset[index]
+        img = read_image(img_path)
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img, pid# , camid, img_path
+
 class ImageDataset(Dataset):
     """Image Person ReID Dataset"""
     def __init__(self, dataset, transform=None):
