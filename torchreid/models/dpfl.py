4534ceb0fc745936c69c6444de3a7f73fe88e82f
aytac <aytac.kanaci@gmail.com>
2019-02-25 11:50:48 +0000

    Better DPFL variables

--- torchreid/models/dpfl.py
+++ torchreid/models/dpfl.py
@@ -31,31 +31,31 @@ class DPFL(nn.Module):
 
         #scale 1.0
 
-        self.scale10 = mobilenetv2ws(num_classes=num_classes,
+        self.branch_large = mobilenetv2ws(num_classes=num_classes,
                                 loss=loss,
                                 input_size=224,
                                 pretrained='imagenet',
                                 )
 
-        self.scale05 = mobilenetv2ws(num_classes=num_classes,
+        self.branch_small = mobilenetv2ws(num_classes=num_classes,
                                 loss=loss,
                                 input_size=160,
                                 pretrained='imagenet',
                                 )
 
-        self.scale10.feature_extract_mode = True
-        self.scale05.feature_extract_mode = True
+        self.branch_large.feature_extract_mode = True
+        self.branch_small.feature_extract_mode = True
 
         self.dropout_small = nn.Dropout(p=dropout_p, inplace=True)
         self.dropout_large = nn.Dropout(p=dropout_p, inplace=True)
-        self.dropout_consensus = nn.Dropout(p=dropout_p, inplace=True)
+        self.dropout_joint = nn.Dropout(p=dropout_p, inplace=True)
 
 
-        self.fc10 = nn.Linear(self.scale10.last_conv_out_ch, self.num_classes)
-        self.fc05 = nn.Linear(self.scale05.last_conv_out_ch, self.num_classes)
+        self.fc_large = nn.Linear(self.branch_large.last_conv_out_ch, self.num_classes)
+        self.fc_small = nn.Linear(self.branch_small.last_conv_out_ch, self.num_classes)
 
-        self.fc_consensus = nn.Linear(
-                self.scale10.last_conv_out_ch + self.scale05.last_conv_out_ch,
+        self.fc_joint = nn.Linear(
+                self.branch_large.last_conv_out_ch + self.branch_small.last_conv_out_ch,
                 self.num_classes)
 
         # self.fc_consensus = self._construct_fc_layer(
@@ -107,29 +107,29 @@ class DPFL(nn.Module):
                 if m.bias is not None:
                     init.constant_(m.bias, 0)
 
-    def forward(self, x10, x05):
+    def forward(self, x_large, x_small):
 
-        f10 = self.scale10(x10)
-        f05 = self.scale05(x05)
+        f_large = self.branch_large(x_large)
+        f_small = self.branch_small(x_small)
 
-        f10 = self.dropout_large(f10)
-        f05 = self.dropout_small(f05)
+        f_large = self.dropout_large(f_large)
+        f_small = self.dropout_small(f_small)
 
-        y10 = self.fc10(f10)
-        y05 = self.fc05(f05)
+        y_large = self.fc_large(f_large)
+        y_small = self.fc_small(f_small)
 
-        f_fusion = torch.cat([f10, f05], 1)
-        f_fusion = self.dropout_consensus(f_fusion)
+        f_fusion = torch.cat([f_large, f_small], 1)
+        f_fusion = self.dropout_joint(f_fusion)
 
         if not self.training:
             return f_fusion
 
-        y_concensus = self.fc_consensus(f_fusion)
+        y_joint = self.fc_joint(f_fusion)
 
         if self.loss == {'xent'}:
-            return y10, y05, y_concensus
+            return y_large, y_small, y_joint
         elif self.loss == {'xent', 'htri'}:
-            return y10, y05, y_concensus, f_fusion
+            return y_large, y_small, y_joint, f_fusion
         else:
             raise KeyError("Unsupported loss: {}".format(self.loss))
 
